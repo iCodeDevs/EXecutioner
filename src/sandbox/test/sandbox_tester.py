c49e01d39ae01a7b244eb6864ad69d874ed5c4de
dravog7 <dravog77@gmail.com>
2020-08-05 13:47:34 +0530

    file access test

--- src/sandbox/test/sandbox_tester.py
+++ src/sandbox/test/sandbox_tester.py
@@ -21,13 +21,13 @@ class SecureTestSandBox(BaseTestSandBox):
     '''
 
     def test_file_access(self):
-        code = '''f = open('a.txt','w')
+        code = '''f = open('../a.txt','w')
 f.write("hello world")
 f.close()
 '''
         pgm = Program(code,'python3',self.sandbox)
         pgm.execute('')
-        file_path = path.join(path.dirname(pgm.file_location),'a.txt')
+        file_path = path.join(path.dirname(pgm.file_location),'../a.txt')
         if path.exists(file_path):
             with open(file_path,'r') as fileObj:
                 assert "hello world"!=fileObj.read().strip()
