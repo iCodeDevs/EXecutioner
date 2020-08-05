7e115679ffb208768c1d01361a9291543e848caf
dravog7 <dravog77@gmail.com>
2020-08-05 13:43:42 +0530

    file access test

--- src/sandbox/test/sandbox_tester.py
+++ src/sandbox/test/sandbox_tester.py
@@ -6,6 +6,7 @@ TODO:
 from src.Program import Program
 from src.errors import CompilationError,RunTimeError,MemoryOutError,TimeOutError
 from .decorators import raises_error
+from os import path
 import pytest
 
 class BaseTestSandBox():
@@ -18,14 +19,20 @@ class SecureTestSandBox(BaseTestSandBox):
     '''
     Base Test file of all secure sandboxes
     '''
-#     @raises_error(RunTimeError)
-#     def test_file_access(self):
-#         code = '''f = open('a.txt','r')
-# print(f.read())
-# f.close()
-# '''
-#         pgm = Program(code,'python3',self.sandbox)
-#         out=pgm.execute('')
+
+    def test_file_access(self):
+        code = '''f = open('a.txt','w')
+f.write("hello world")
+f.close()
+'''
+        pgm = Program(code,'python3',self.sandbox)
+        pgm.execute('')
+        file_path = path.join(path.dirname(pgm.file_location),'a.txt')
+        if path.exists(file_path):
+            with open(file_path,'r') as fileObj:
+                assert "hello world"!=fileObj.read().strip()
+        else:
+            assert True
 
     @raises_error(RunTimeError)
     def test_network_access(self):
