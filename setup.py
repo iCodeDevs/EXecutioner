2f8d88d7f0db922be455e567eef66b6d4c41bee0
dravog7 <dravog78@gmail.com>
2021-09-18 11:08:14 +0000

    fixing dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyExecutioner",
-    version="0.0.4",
+    version="0.0.5",
     author="dravog",
     author_email="dravog78@gmail.com",
     description="A library to execute code",
@@ -25,7 +25,7 @@ setuptools.setup(
     install_requires=[
         "setuptools>=42",
         "wheel",
-        "deepmerge",
+        "deepmerge==0.1.0",
         "PyYAML",
         "tabulate",
     ],
