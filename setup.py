5b9f440a97f8fb37f0b0c210b0f725611861963b
dravog7 <dravog78@gmail.com>
2021-09-18 11:58:34 +0000

    first pypi release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyExecutioner",
-    version="0.0.9",
+    version="0.0.10",
     author="dravog",
     author_email="dravog78@gmail.com",
     description="A library to execute code",
